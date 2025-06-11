d687b4262cd8ca1d3bc7c766ec9381ad3c5cd9e5
Omegea Hunter <omegeadhunter@gmail.com>
2025-06-11 12:49:10 -0400

    implement function to return the first ten natural numbers

--- exercises/e1.js
+++ exercises/e1.js
@@ -5,7 +5,11 @@
 
 export function getFirstTenNumbers() {
   // Your code goes here...
-
+    const numbers = [];
+    for (let i = 1; i <= 10; i++) {
+        numbers.push(i);
+    }
+    return numbers;
 }
 
 // === TEST YOURSELF ===
