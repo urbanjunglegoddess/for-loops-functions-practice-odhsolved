e8563f3de92b60a1d8962c8753da34b327d11570
Omegea Hunter <omegeadhunter@gmail.com>
2025-06-12 14:31:55 -0400

    implement getAllWithdrawals function to calculate total withdrawals for each bank account

--- exercises/e12.js
+++ exercises/e12.js
@@ -6,7 +6,18 @@
 
 export function getAllDepositsGreaterThanOneHundred(array) {
   // Your code goes here...
-
+    let result = [];
+    for(let i = 0; i < array.length; i++) {
+        const deposits = array[i].deposits;
+        if ( Array.isArray (deposits))  {
+          for (let j = 0; j < deposits.length; j++) {
+            if (deposits[j] > 100) {
+              result.push(deposits[j]);
+            }
+          }
+                }
+    }
+return result;
 }
 
 
