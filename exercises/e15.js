6cf64f1ed37f2876b0de93e4b0de4882b69ed539
Omegea Hunter <omegeadhunter@gmail.com>
2025-06-16 14:15:27 -0400

    implement concatArrays function to merge two arrays into one

--- exercises/e15.js
+++ exercises/e15.js
@@ -6,9 +6,16 @@
 
 export function concatArrays(arr1, arr2) {
   // Your code goes here...
-
+    let result = [];
+for (let i = 0; i <arr1.length; i ++){
+    result.push(arr1[i]);
 }
+for (let j = 0; j < arr2.length; j++) {
+    result.push(arr2[j]);
+  }
 
+return result;
+}
 
 
 // === TEST YOURSELF ===
