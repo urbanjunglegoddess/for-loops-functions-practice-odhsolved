ad2ff68b0ea04fae3083ba23ab2ea302c89a85d5
Omegea Hunter <omegeadhunter@gmail.com>
2025-06-16 14:07:21 -0400

    implement getClientsWithWrongBalance function to identify accounts with incorrect balance based on deposits and withdrawals

--- exercises/e14.js
+++ exercises/e14.js
@@ -6,11 +6,47 @@
 // getClientsWithWrongBalance(bankAccounts) => [{ name: 'Name1', balance: 32, ... }, { name: 'Name2', balance: 3523, ... }]
 
 export function getClientsWithWrongBalance(array) {
-  // Your code goes here...
+    // Your code goes here...
+// An empty array to store the accounts with wrong balance
+    let result = [];
+    //Iterate over the array of bank accounts
+    for ( let i = 0 ; i < array.length ; i++ ) {
+        // Check if the account has a balance property
+        // if ( array.balance === 0 ) {
+        //     return `No balance found for account ${ array[i].name }`;
+        // }
+        //Make the sum of deposits and withdrawals into variables
+        const depositSum = sumDeposits(array[i].deposits || []);
+        const withdrawalSum = sumWithdrawals(array[i].withdrawals || []);
+
+        // Calculate the sum of deposits and withdrawals
+        if ( depositSum - withdrawalSum !== array[i].balance ) {
+            // If the balance is wrong, push the account to the result array
+            result.push ( array[i] )
+        }
+        // If the balance is correct, do nothing
+
+    };
+return result;
 
 }
 
 
+const sumDeposits = ( deposits ) => {
+    let sum = 0;
+    for ( let j = 0 ; j < deposits.length ; j++ ) {
+        sum += deposits[j];
+    }
+    return sum;
+};
+
+const sumWithdrawals = ( withdrawals ) => {
+    let sum = 0;
+    for ( let j = 0 ; j < withdrawals.length ; j++ ) {
+        sum += withdrawals[j];
+    }
+    return sum;
+}
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-14"
